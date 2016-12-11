9fd661de266bc683b79ab92882ade96d75fe602a
Charlotte Lily Fields <Foxsan48@gmail.com>
2016-12-11 21:40:06 +1000

    Init

---
+++ c/sea2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("\n\n\n\n\n\n\n\n\n\n");
+  printf("            ******************\n");
+  printf("            *  From sea      *\n");
+  printf("            *  to shining C  *\n");
+  printf("            ******************\n");
+  printf("\n\n\n\n\n\n\n\n\n\n");
+  return 0;
+}
